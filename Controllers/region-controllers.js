4dd781129acb60b2eeee38e58da82bd63b9a5f76
Allv <wijayairawan123@gmail.com>
2024-02-23 23:19:00 +0700

    minor bugs

--- Controllers/region-controllers.js
+++ Controllers/region-controllers.js
@@ -35,7 +35,7 @@ const getAllRegion = async (req, res, next) => {
   try {
     const regions = await Region.findOne();
 
-    res.status(200).json({ status: "ok", regions: regions });
+    res.status(200).json({ status: "ok", regions: regions.regions });
   } catch (e) {
     return next(new Error(e, 400));
   }
